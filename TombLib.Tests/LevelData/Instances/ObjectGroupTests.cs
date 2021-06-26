598d8d419748e1367acdca952cf0d600b4467fec
Wojtek Wilimowski <wjwilimowski@gmail.com>
2021-06-26 22:47:04 +0200

    Add SetPosition tests

--- TombLib.Tests/LevelData/Instances/ObjectGroupTests.cs
+++ TombLib.Tests/LevelData/Instances/ObjectGroupTests.cs
@@ -33,5 +33,33 @@ namespace TombLib.Tests.LevelData.Instances
                 first => Assert.Same(obj, first)
                 );
         }
+
+        [Fact]
+        public void SetPosition_SetsRelativeChildPositions()
+        {
+            // Arrange
+            var position1 = new Vector3(100f, 200f, 300f);
+            var position2 = new Vector3(105f, 205f, 305f);
+            var position3 = new Vector3(95f, 195f, 295f);
+
+            var obj1 = new MoveableInstance { Position = position1 };
+            var obj2 = new MoveableInstance { Position = position2 };
+            var obj3 = new MoveableInstance { Position = position3 };
+
+            var objectGroup = new ObjectGroup(new[] { obj1, obj2, obj3 });
+
+            // Act
+            var targetPosition = new Vector3(50f, 60f, 70f);
+            objectGroup.SetPosition(targetPosition);
+
+            // Assert
+            var expectedPosition2 = new Vector3(55f, 65f, 75f);
+            var expectedPosition3 = new Vector3(45f, 55f, 65f);
+
+            Assert.Equal(targetPosition, objectGroup.Position);
+            Assert.Equal(targetPosition, obj1.Position);
+            Assert.Equal(expectedPosition2, obj2.Position);
+            Assert.Equal(expectedPosition3, obj3.Position);
+        }
     }
 }
