d5fe458681c49da1b5d5e9450e63aa97edb3db94
Wojtek Wilimowski <wjwilimowski@gmail.com>
2021-07-31 17:46:53 +0200

    Update ObjectGroup tests

--- TombLib.Tests/LevelData/Instances/ObjectGroupTests.cs
+++ TombLib.Tests/LevelData/Instances/ObjectGroupTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 using TombLib.LevelData;
 using Xunit;
 
@@ -45,10 +46,12 @@ namespace TombLib.Tests.LevelData.Instances
             var position3 = new Vector3(95f, 195f, 295f);
 
             var obj1 = new MoveableInstance { Position = position1 };
-            var obj2 = new MoveableInstance { Position = position2 };
-            var obj3 = new MoveableInstance { Position = position3 };
+            var obj2 = new LightInstance(LightType.Spot) { Position = position2 };
+            var obj3 = new SinkInstance { Position = position3 };
 
-            var objectGroup = new ObjectGroup(new[] { obj1, obj2, obj3 });
+            var objects = new List<PositionBasedObjectInstance> { obj1, obj2, obj3 };
+
+            var objectGroup = new ObjectGroup(objects);
 
             // Act
             var targetPosition = new Vector3(50f, 60f, 70f);
@@ -65,7 +68,7 @@ namespace TombLib.Tests.LevelData.Instances
         }
 
         [Fact]
-        public void SetRotation_AdjustsChildRotationsByDifference()
+        public void SetRotation_AdjustsChildRotationsByDifference_ForIRotatableYChildren()
         {
             // Arrange
             var rotation1 = 70f;
@@ -73,10 +76,13 @@ namespace TombLib.Tests.LevelData.Instances
             var rotation3 = 110f;
 
             var obj1 = new MoveableInstance { RotationY = rotation1 };
-            var obj2 = new MoveableInstance { RotationY = rotation2 };
+            var obj2 = new LightInstance(LightType.Spot) { RotationY = rotation2 };
             var obj3 = new MoveableInstance { RotationY = rotation3 };
+            var obj4 = new SinkInstance();
 
-            var objectGroup = new ObjectGroup(new[] { obj1, obj2, obj3 });
+            var objects = new List<PositionBasedObjectInstance> { obj1, obj2, obj3, obj4 };
+
+            var objectGroup = new ObjectGroup(objects);
 
             // Act
             var targetRotation = 15f;
@@ -111,10 +117,12 @@ namespace TombLib.Tests.LevelData.Instances
             var rotation3 = 90f; var position3 = new Vector3(1f, 3f, 2f);
 
             var obj1 = new MoveableInstance { RotationY = rotation1, Position = position1 };
-            var obj2 = new MoveableInstance { RotationY = rotation2, Position = position2 };
-            var obj3 = new MoveableInstance { RotationY = rotation3, Position = position3 };
+            var obj2 = new FlybyCameraInstance { RotationY = rotation2, Position = position2 };
+            var obj3 = new LightInstance(LightType.Spot) { RotationY = rotation3, Position = position3 };
 
-            var objectGroup = new ObjectGroup(new[] { obj1, obj2, obj3 });
+            var objects = new List<PositionBasedObjectInstance> { obj1, obj2, obj3 };
+
+            var objectGroup = new ObjectGroup(objects);
 
             // Act
             var targetRotation = -90f;  // Clockwise
@@ -137,6 +145,63 @@ namespace TombLib.Tests.LevelData.Instances
             Assert.Equal(expectedRotation1, obj1.RotationY);
             Assert.Equal(expectedRotation2, obj2.RotationY);
             Assert.Equal(expectedRotation3, obj3.RotationY);
+
+            Assert.Equal(expectedPosition1, obj1.Position);
+            Assert.Equal(expectedPosition2, obj2.Position);
+            Assert.Equal(expectedPosition3, obj3.Position);
+
+            Assert.Equal(targetRotation, objectGroup.RotationY);
+        }
+
+
+        [Fact]
+        public void RotateAsGroup_DoesNotSetRotationIfChildNotIRotatableY()
+        {
+            /*
+             * Assuming one dot is 1 unit of distance and object 1 is at X: -1, Z: 1
+             * X axis is horizontal, axes increase top-left to bottom-right
+             *
+             * Before:
+             * . . . . .
+             * . . 2 . .
+             * . . 1 . .
+             * . . . . 3
+             * . . . . .
+             */
+
+            // Arrange
+            var rotation1 = 70f; var position1 = new Vector3(-1f, 1f, 1f);
+            var rotation2 = 20f; var position2 = new Vector3(-1f, 2f, 0f);
+            var position3 = new Vector3(1f, 3f, 2f);
+
+            var obj1 = new MoveableInstance { RotationY = rotation1, Position = position1 };
+            var obj2 = new MoveableInstance { RotationY = rotation2, Position = position2 };
+            var obj3 = new SinkInstance() { Position = position3 };
+
+            var objects = new List<PositionBasedObjectInstance> { obj1, obj2, obj3 };
+
+            var objectGroup = new ObjectGroup(objects);
+
+            // Act
+            var targetRotation = -90f;  // Clockwise
+            objectGroup.RotateAsGroup(targetRotation);
+
+            // Assert
+
+            /*
+             * After:
+             * . . . . .
+             * . . . . .
+             * . . 1 2 .
+             * . . . . .
+             * . 3 . . .
+             */
+            var expectedRotation1 = 340f; var expectedPosition1 = position1;
+            var expectedRotation2 = 290f; var expectedPosition2 = new Vector3(0f, 2f, 1f);
+            var expectedPosition3 = new Vector3(-2f, 3f, 3f);
+
+            Assert.Equal(expectedRotation1, obj1.RotationY);
+            Assert.Equal(expectedRotation2, obj2.RotationY);
 
             Assert.Equal(expectedPosition1, obj1.Position);
             Assert.Equal(expectedPosition2, obj2.Position);
